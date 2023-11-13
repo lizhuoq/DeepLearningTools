956631aec40177327d1cabcddf1e1bdedfe500fd
Li zhuoqun <m15004059308@163.com>
2023-11-13 14:56:23 +0800

    feat: add custom & numpy pearson metrics

---
+++ metrics.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from package import *
+
+
+def custom_pearson(x, y):
+    x_mean = x.mean()
+    y_mean = y.mean()
+    x_minus_mean = (x - x_mean)
+    y_minus_mean = (y - y_mean)
+    x_minus_mean_square = x_minus_mean ** 2
+    y_minus_mean_square = y_minus_mean ** 2
+    return (x_minus_mean * y_minus_mean).sum() / np.sqrt(x_minus_mean_square.sum() * y_minus_mean_square.sum())
+
+
+def numpy_pearson(x, y):
+    return np.corrcoef(x, y)[0][1]
+
+
