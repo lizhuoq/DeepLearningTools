72a9e667b8a9d3c786567ab96bc99e50e074004a
Li zhuoqun <m15004059308@163.com>
2023-11-14 17:14:29 +0800

    fix: fix bug in structured_data_utils.py

--- package.py
+++ package.py
@@ -4,6 +4,9 @@ from torch import nn
 from torch.nn import functional as F
 import pandas as pd
 from flaml import AutoML
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import seaborn as sns
 
 from typing import List
 import doctest
--- structured_data_utils.py
+++ structured_data_utils.py
@@ -45,38 +45,39 @@ def plot_corr(df, columns):
     mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
 
     plt.figure(figsize=(15, 12))
-    sns.heatmap(corr_matrix, mask=mask, annot=True, cmap='Blues', fmt='.2f', linewidths=1, square=True, annot_kws={"size": 9})
+    sns.heatmap(corr_matrix, mask=mask, annot=True, cmap='Blues', fmt='.2f', linewidths=1, square=True,
+                annot_kws={"size": 9})
     plt.title('Correlation Matrix', fontsize=15)
     plt.show()
 
 
 def plot_train_test_dis(df_train, df_test, numeric_columns):
-    fig, axes = plt.subplots(len(numeric_columns), 3 ,figsize = (16, len(numeric_columns) * 4.2),
-                         gridspec_kw = {'hspace': 0.35, 'wspace': 0.3, 'width_ratios': [0.80, 0.20, 0.20]})
+    fig, axes = plt.subplots(len(numeric_columns), 3, figsize=(16, len(numeric_columns) * 4.2),
+                             gridspec_kw={'hspace': 0.35, 'wspace': 0.3, 'width_ratios': [0.80, 0.20, 0.20]})
 
     for i, col in tqdm(enumerate(numeric_columns)):
         ax = axes[i, 0]
-        sns.kdeplot(data=df_train, x=col, ax=ax, linewidth = 2.1, label='Train')
-        sns.kdeplot(data=df_test, x=col, ax=ax, linewidth = 2.1, label='Test')
-        ax.set_title(f"\n{col}",fontsize = 9, fontweight= 'bold')
-        ax.grid(visible=True, which = 'both', linestyle = '--', color='lightgrey', linewidth = 0.75)
-        ax.set(xlabel = '', ylabel = '')
+        sns.kdeplot(data=df_train, x=col, ax=ax, linewidth=2.1, label='Train')
+        sns.kdeplot(data=df_test, x=col, ax=ax, linewidth=2.1, label='Test')
+        ax.set_title(f"\n{col}", fontsize=9, fontweight='bold')
+        ax.grid(visible=True, which='both', linestyle='--', color='lightgrey', linewidth=0.75)
+        ax.set(xlabel='', ylabel='')
         ax.legend()
 
         ax = axes[i, 1]
-        sns.boxplot(data=df_train, y=col, width = 0.25,saturation = 0.90, linewidth = 0.90, fliersize= 2.25, color = '#037d97',
-                ax = ax)
-        ax.set(xlabel = '', ylabel = '')
-        ax.set_title(f"Train",fontsize = 9, fontweight= 'bold')
+        sns.boxplot(data=df_train, y=col, width=0.25, saturation=0.90, linewidth=0.90, fliersize=2.25, color='#037d97',
+                    ax=ax)
+        ax.set(xlabel='', ylabel='')
+        ax.set_title(f"Train", fontsize=9, fontweight='bold')
 
         ax = axes[i, 2]
-        sns.boxplot(data=df_test, y = col, width = 0.25, fliersize= 2.25,
-                saturation = 0.6, linewidth = 0.90, color = '#E4591E',
-                ax = ax)
-        ax.set(xlabel = '', ylabel = '')
-        ax.set_title(f"Test",fontsize = 9, fontweight= 'bold')
+        sns.boxplot(data=df_test, y=col, width=0.25, fliersize=2.25,
+                    saturation=0.6, linewidth=0.90, color='#E4591E',
+                    ax=ax)
+        ax.set(xlabel='', ylabel='')
+        ax.set_title(f"Test", fontsize=9, fontweight='bold')
 
-    plt.suptitle(f"\nDistribution analysis- continuous columns\n",fontsize = 12, fontweight= 'bold',
-             y = 0.89, x = 0.57)
+    plt.suptitle(f"\nDistribution analysis- continuous columns\n", fontsize=12, fontweight='bold',
+                 y=0.89, x=0.57)
     plt.tight_layout()
     plt.show()
